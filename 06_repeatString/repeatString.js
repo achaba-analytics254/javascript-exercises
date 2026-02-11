7024e5c29ee2300650d8bd0b98e253e46994923f
achaba-analytics254 <djangozent@gmail.com>
2026-02-12 02:54:35 +0300

    repeat  string

---
+++ 06_repeatString/repeatString.js
@@ -0,0 +1,12 @@
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR'
+
+    let myWord = '';
+    for (let i = 0; i < num; i++){
+        string += myWord;
+    }
+    return 
+};
+
+// Do not edit below this line
+module.exports = repeatString;
