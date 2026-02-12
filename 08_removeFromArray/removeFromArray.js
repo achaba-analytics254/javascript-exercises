1b2e49a3e54b1aa54bf6e0af89ae2676736bef69
achaba-analytics254 <djangozent@gmail.com>
2026-02-12 03:54:09 +0300

    remove values

---
+++ 08_removeFromArray/removeFromArray.js
@@ -0,0 +1,6 @@
+const removeFromArray = function(arr, ...valuesToRemove) {
+    return arr.filter(item => valuesToRemove.includes(item));
+};
+
+// Do not edit below this line
+module.exports = removeFromArray;
