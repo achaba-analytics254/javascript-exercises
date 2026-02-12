63393d6b0270ac787130be1fae0f3306f3cac444
achaba-analytics254 <djangozent@gmail.com>
2026-02-12 05:24:16 +0300

    temp conversion

---
+++ 11_tempConversion/tempConversion.js
@@ -0,0 +1,13 @@
+const convertToCelsius = function(temp) {
+  return (temp * 9 / 5 - 32).toFixed(1);
+};
+
+const convertToFahrenheit = function(temp) {
+  return (temp - 32) * (5 / 9).toFixed(1);
+};
+
+// Do not edit below this line
+module.exports = {
+  convertToCelsius,
+  convertToFahrenheit
+};
